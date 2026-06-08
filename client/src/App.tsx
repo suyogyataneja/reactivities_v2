7c17c396de4d2edbbd690797447f5442fb91af3c
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-06-08 23:03:04 +1000

    Initialize React client with Vite, TypeScript, and Axios for API integration.

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import './App.css'
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
+import axios from 'axios';
+
+function App() {
+  
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+    .then(response => setActivities(response.data))
+
+    return () =>{}
+    
+  },[])
+
+  return (
+<>
+     <Typography variant='h3'>Reactivities</Typography>
+     <List>
+        {activities.map((activity)=>(
+        <ListItem key={activity.id}>
+          
+          <ListItemText>{activity.title}</ListItemText>
+ 
+          </ListItem>
+
+        ))}
+     </List>
+</>
+ 
+  )
+}
+
+export default App
---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+ type Activity = {
+    id: string
+    title: string
+    description: string
+    category: string
+    date: string
+    isCancelled: boolean
+    city: string
+    venue: string
+    latitude: number
+    longitude: number
+}
